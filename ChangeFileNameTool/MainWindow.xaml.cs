6ac5706748c53a6670ab72f548a5552669c508c0
Heo SuYeong <hykeegj@gmail.com>
2020-06-27 14:41:10 +0900

    Ver 1.0.0

--- ChangeFileNameTool/MainWindow.xaml.cs
+++ ChangeFileNameTool/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace ChangeFileNameTool
             InitializeComponent();
         }
 
-        // 오름차순 내림차순 정렬 유무 판단 bool 변수
+        // 오름차순 내림차순 정렬 유무 판단 bool 변수 d
         bool isSortedVideoFileNameFlag = false;
         bool isSortedVideoFileExtensionFlag = false;
         bool isSortedSubtitleFileNameFlag = false;
