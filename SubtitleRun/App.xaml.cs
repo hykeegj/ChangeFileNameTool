810b858c9a096476bfa18d13364e89a16a8997a2
Heo SuYeong <hykeegj@gmail.com>
2020-07-26 15:35:32 +0900

    자막런 2.0.0 업데이트
    SubtitleRun 2.0.0 Update!

--- SubtitleRun/App.xaml.cs
+++ SubtitleRun/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ChangeFileNameTool
+namespace SubtitleRun
 {
     /// <summary>
     /// Interaction logic for App.xaml
